8c91d03ffe1b3253f8dffed71772ecc67e242cc0
Benjamin Jukus <jukusj0@students.rowan.edu>
2017-09-11 11:33:19 -0400

    Initial Commit

---
+++ Assignment/README_lab_1.txt
@@ -0,0 +1,16 @@
+README, Lab 1 
+Author: Ben Jukus
+9/9/2017
+Math.h is functioning based on a sample int main made to 
+	solely test its individual functionality. All 
+	documentation is in math.h in its comments. Math.h
+	uses no libraries, modulus as it turns out isn't 
+	in an external library.
+	Just make sure you put in real numbers and the correct 
+	characters for operator. Invalid numbers will throw errors,
+	like trying to divide by 0. For modulus use num2 as the dividend
+	and num1 as the divisor((num1=5)%(num2=2)=(result=1)).
+No dev boards to test any of the example code just yet. 
+	They should be arriving soon. Updates to the README 
+	might be made a progress is. 
+
--- Assignment/math.h
+++ Assignment/math.h
@@ -1,23 +1,78 @@
 /*
- * math.h
- *
- *  Created on: Aug 22, 2017
- * 	Last Edited: Sept 5, 2017
- *      Author: Russell Trafford
- */
-
-/* Your assignment is to take the math function and implement at least the following functions:
- * + Add (num1 + num2)
- * - Subtract (num1 - num2)
- * * Multiply (num1 * num2)
- * / Divide (num1 / num2)
- * % Modulus (num1 % num2)
-*/
-
+ Author: Ben Jukus
+ Status: Math header works when numbers are fed in
+ Issues: Test main.c I wasn't able to figure out how to get scanf and int math(...) to work together to get an expected outcome. 
+			Few issues with syntax, all resolved save for scanf. To my knowledge my syntax is correct.
+ Resolution: Take the l on the main.c. Math.h functions as desired.
+ Push: 1 (First Push)
+ */  
+  
 #ifndef MATH_H_
 #define MATH_H_
 
-//Part of your documentation should be listing the valid inputs and outputs for the functions you create.
-int math(int num1, int num2, char Operator);
+int math (int num1, int num2, char Operator) 
 
-#endif /* MATH_H_ */
+{
+
+float result;
+  
+int i = 0;
+  
+switch (Operator)
+    
+    {
+    
+case ('+'):		//any number should be valid
+      
+result = (num1 + num2);
+      
+	//printf(result);
+	break;
+    
+ 
+case ('-'):		//if num1 is larger than num2 result will return a negative number but all real numbers are valid
+      result = (num1 - num2);
+      
+	//      printf(result);
+	break;
+    
+ 
+case ('*'):
+      
+result = (num1 * num2);
+      
+	//      printf(result);
+	break;
+    
+ 
+case ('/'):		//num1 is divided by num2
+      //Don't try to divide by zero you hecker
+      
+result = (num1 / num2);
+      
+ 
+	//      printf(result);
+	break;
+    
+ 
+case ('%'):		//num2 is the dividend and num1 is the divisor
+      //enter num2 and num1 as such
+      result = num1 % num2;
+      
+break;
+    
+ 
+default:
+      
+printf ("Invalid operation"); //this is all that prints trying to use scanf. 
+    
+ 
+}
+  
+return result;
+
+}
+
+
+ 
+#endif	/* MATH_H_ */
